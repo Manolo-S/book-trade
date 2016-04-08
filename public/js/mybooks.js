9276bf9c9fd7dba3141ebf3113e86c5ff9b54605
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-04-07 21:57:45 -0300

    webpack works

--- public/js/mybooks.js
+++ public/js/mybooks.js
@@ -1,4 +1,8 @@
 'use strict';
 console.log('mybooks.js called');
+window.$ = window.jQuery = require('../lib/jQuery.2.2.1.js');
+require('../lib/bootstrap.3.3.6.js');
+require('../lib/bootstrap.3.3.6.css');
+require('../lib/font-awesome.min.css');
 
-require ('../css/mybooks.css');
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ var path = require('path');
 module.exports = {
 	context: path.resolve('./public/js'),
 	entry: {
-			index: './index.js',
-			mybooks: './mybooks.js'
-			},
+		index: './index.js',
+		mybooks: './mybooks.js'
+	},
 	output: {
 		path: path.resolve('build/js'),
 		publicPath: '/public/assets/js/',
@@ -16,29 +16,29 @@ module.exports = {
 		contentBase: 'public'
 	},
 	module: {
-		preLoaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'eslint-loader'
-			}
-		],
-		loaders: [
-			{
-				test: /\.es6$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			},
-			{
-				test: /\.css$/,
-				exclude: /node_modules/,
-				loader: "style-loader!css-loader"
-			}
-		]
+		preLoaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'eslint-loader'
+		}],
+		loaders: [{
+			test: /\.es6$/,
+			exclude: /node_modules/,
+			loader: "babel-loader"
+		}, {
+			test: /\.css$/,
+			exclude: /node_modules/,
+			loader: "style-loader!css-loader"
+		}, {
+			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "url?limit=370000&minetype=application/font-woff"
+		}, {
+			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "file"
+		}]
 	},
 
 	resolve: {
 		extensions: ['', '.js', '.es6']
 	}
 }
-
