2c0a4fd9c5608cd412029a2ae2812abc57b47f95
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-04-08 10:48:35 -0300

    add search box

--- public/js/mybooks.js
+++ public/js/mybooks.js
@@ -1,8 +1,9 @@
 'use strict';
-console.log('mybooks.js called');
+console.log('mybooks.js called again');
 window.$ = window.jQuery = require('../lib/jQuery.2.2.1.js');
 require('../lib/bootstrap.3.3.6.js');
 require('../lib/bootstrap.3.3.6.css');
 require('../lib/font-awesome.min.css');
+require('../css/mybooks.css');
 
 
